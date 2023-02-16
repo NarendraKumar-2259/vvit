0f3814ff623fac71e7d5b76e25efd86236700a47
NarendraKumar-2259 <ilavarapunarendra28@gmail.com>
2023-02-16 15:36:56 +0530

    hi

--- src/Ab.java
+++ src/Ab.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Ab {
+public class Ab {
+    public static void main(String[] args) {
+        System.out.println("hello narendra how are you doing");
+        System.out.println("hi how are you all");
+    }
 }
